8f81acc2cc1b608a555aecba624b210f7d4b5dd4
Gary Lilienfield <glilienfield@yahoo.com>
2020-08-02 00:38:23 -0400

    Full baseline version

--- src/main/java/com/GfeeService/aws/dynamoDB/das/UpdateGfeeDAS.java
+++ src/main/java/com/GfeeService/aws/dynamoDB/das/UpdateGfeeDAS.java
@@ -38,48 +38,33 @@ public class UpdateGfeeDAS {
     public void updateSellerProduct(String sellerNumber, int productCode, BigDecimal value) {
         Gfee_Live gfee_live = gfeeLiveDAS.getProductGfeeForSellerNo(sellerNumber, productCode);
         Gfee_Hist gfee_hist = gfeeHistDAS.getProductGfeeForSellerNo(sellerNumber, productCode, new BigInteger("99999999999999"));
-
         Gfee_Hist gfee_hist_next = gfee_hist.duplicate();
-        BigDecimal current_live_gfee = gfee_live.getGfee();
-        BigDecimal current_hist_gfee = gfee_hist.getHistValue();
-        BigInteger current_epoc_time = gfee_hist.getEndEpocTime();
 
         BigInteger new_epoc_time = BigInteger.valueOf((Instant.from(ZonedDateTime.now()).toEpochMilli())).add(BigInteger.valueOf(10000));
+
+        Map<String, AttributeValue> conditionLiveExpressionValues = new HashMap<>();
+        conditionLiveExpressionValues.put(":current_live_gfee_value", new AttributeValue().withN(gfee_live.getGfee().toString()));
+        conditionLiveExpressionValues.put(":current_hist_gfee_value", new AttributeValue().withN(gfee_hist.getHistValue().toString()));
+
         gfee_live.setGfee(value);
         gfee_hist.setEndEpocTime(new_epoc_time);
         gfee_hist_next.setHistValue(value);
         gfee_hist_next.setStartEpocTime(new_epoc_time.add(BigInteger.valueOf(1)));
-//        myDynamoDBMapperClient.batchSave(Arrays.asList(gfee_live, gfee_hist, gfee_hist_next));
-
-//        BigInteger newEndEpoc = BigInteger.valueOf((Instant.from(ZonedDateTime.now()).toEpochMilli())).add(BigInteger.valueOf(10000));
-//        BigInteger newStartEpoc = new_epoc_time.add(BigInteger.valueOf(1));
-
-        Map<String, AttributeValue> conditionLiveExpressionValues = new HashMap<>();
-        conditionLiveExpressionValues.put(":current_live_gfee_value", new AttributeValue().withN(current_live_gfee.toString()));
-        conditionLiveExpressionValues.put(":current_hist_gfee_value", new AttributeValue().withN(current_hist_gfee.toString()));
-
-        Map<String, AttributeValue> conditionHistExpressionValues = new HashMap<>();
-        conditionHistExpressionValues.put(":current_hist_gfee_value", new AttributeValue().withN(current_hist_gfee.toString()));
-//        conditionHistExpressionValues.put(":current_epoc_time", new AttributeValue().withN(current_epoc_time.toString()));
 
         DynamoDBTransactionWriteExpression gfeeLiveUnchanged = new DynamoDBTransactionWriteExpression()
-                .withConditionExpression("attribute_exists(sellerNumber)")
-                .withConditionExpression("attribute_exists(productCode)")
-                .withConditionExpression("gfee = :current_live_gfee_value")
+                .withConditionExpression("attribute_exists(gfee)")
                 .withConditionExpression(":current_live_gfee_value = :current_hist_gfee_value")
                 .withExpressionAttributeValues(conditionLiveExpressionValues);
 
         DynamoDBTransactionWriteExpression gfeeHistUnchanged = new DynamoDBTransactionWriteExpression()
-                .withConditionExpression("attribute_exists(sellerNumber)")
-                .withConditionExpression("attribute_exists(productCode)");
-//                .withConditionExpression("epocEndTime = :current_epoc_time")
-//                .withConditionExpression("histValue = :current_hist_gfee_value")
-//                .withExpressionAttributeValues(conditionHistExpressionValues);
+                .withConditionExpression("attribute_exists(histValue)")
+                .withConditionExpression("attribute_exists(startEpocTime)");
 
-        TransactionWriteRequest transactionWriteRequest = new TransactionWriteRequest();
-        transactionWriteRequest.addUpdate(gfee_live, gfeeLiveUnchanged);
-        transactionWriteRequest.addUpdate(gfee_hist);
-        transactionWriteRequest.addUpdate(gfee_hist_next);
+        TransactionWriteRequest transactionWriteRequest = new TransactionWriteRequest()
+                .addUpdate(gfee_live, gfeeLiveUnchanged)
+                .addUpdate(gfee_hist_next, gfeeHistUnchanged)
+                .addPut(gfee_hist);
+
         executeTransactionWrite(transactionWriteRequest);
     }
 
